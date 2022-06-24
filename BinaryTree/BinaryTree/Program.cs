c40b392d6ca58f0cba9060bc8f1aff8e0aa43a6a
guri97 <105732963+guri97@users.noreply.github.com>
2022-06-24 22:35:53 +0530

    [gurpreet] UC1_AddDataToTree

---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Day19_BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Binary Search Tree Problem");
+            BinarySearchTree<int> BinaryTree = new BinarySearchTree<int>();
+
+            ///UC1: Add the node in the tree and it doesnot have any output just display welcome message
+
+            BinaryTree.AddNode(56);
+            BinaryTree.AddNode(30);
+            BinaryTree.AddNode(70);
+            BinaryTree.AddNode(22);
+            BinaryTree.AddNode(40);
+            BinaryTree.AddNode(11);
+            BinaryTree.AddNode(63);
+            BinaryTree.AddNode(67);
+            BinaryTree.AddNode(16);
+            BinaryTree.AddNode(60);
+            BinaryTree.AddNode(95);
+            BinaryTree.AddNode(65);
+            BinaryTree.AddNode(3);
+
+
+        }
+    }
+}
